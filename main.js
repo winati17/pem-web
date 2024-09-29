06ae2ea8d4fc6ae2613db121d94e010635258d7f
winati17 <winatimn@gmail.com>
2024-09-30 00:18:17 +0800

    tidak ngerti git..

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import './style.css'; 
-
 $(document).ready(function () {
   $('#app').html(`
     <div class="container">
@@ -8,10 +7,8 @@ $(document).ready(function () {
         <button id="button">Klik disini</button>
     </div>
   `);
-
   const originalText = $('#heading').text();
   const newText = 'Wah, tulisannya berubah!';
-
   $('#button').click(function () {
     if ($('#heading').text() === originalText) {
       $('#heading').text(newText);
